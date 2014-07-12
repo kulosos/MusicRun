e89b682e528dc7aae59fcd48af0284adc5cb7db4
Oliver Kulas <oli@SEKTOR-7MB.local>
2014-07-12 09:57:50 +0200

    StepDetection with API Level detection
    decides between my own stepDetection method and the stepDetection
    Sensor by googles API Level 19 and higher

--- src/de/thm/fmi/musicrun/pedometer/StepDetector.java
+++ src/de/thm/fmi/musicrun/pedometer/StepDetector.java
@@ -18,154 +18,182 @@ import android.util.Log;
 
 public class StepDetector implements SensorEventListener
 {
-	
+
 	// DEBUG
 	private static final String TAG = MainActivity.class.getName();
-	private static final boolean D = false;
-//    private final static String TAG = "StepDetector";
-    
+	private static final boolean D = true;
+	//    private final static String TAG = "StepDetector";
+
 	public SensorManager sensorManager;
 	private boolean isActivityRunning;
-	
-    private float   mLimit = 10;
-    private float   mLastValues[] = new float[3*2];
-    private float   mScale[] = new float[2];
-    private float   mYOffset;
 
-    private float   mLastDirections[] = new float[3*2];
-    private float   mLastExtremes[][] = { new float[3*2], new float[3*2] };
-    private float   mLastDiff[] = new float[3*2];
-    private int     mLastMatch = -1;
- 
-    private Vector observers;
-    
-    // ------------------------------------------------------------------------
-    
-    public StepDetector(Object systemService) {
-    	if(D)Log.i(TAG, "DEBUG - StepDetector CONSTRUCTOR"); // DEBUG
-    	
-    	this.sensorManager = (SensorManager) systemService;
-    	
-        int h = 480; // TODO: remove this constant
-        mYOffset = h * 0.5f;
-        mScale[0] = - (h * 0.5f * (1.0f / (SensorManager.STANDARD_GRAVITY * 2)));
-        mScale[1] = - (h * 0.5f * (1.0f / (SensorManager.MAGNETIC_FIELD_EARTH_MAX)));
-        
-        // instantiate observer vector
-        observers = new Vector();
-    }
-    
-    public void setSensitivity(float sensitivity) {
-        mLimit = sensitivity; // 1.97  2.96  4.44  6.66  10.00  15.00  22.50  33.75  50.62
-    }
-    
-    // ------------------------------------------------------------------------
+	private float   mLimit = 10;
+	private float   mLastValues[] = new float[3*2];
+	private float   mScale[] = new float[2];
+	private float   mYOffset;
 
-    public void registerSensorManager(){
-    	if(D)Log.i(TAG, "DEBUG - StepDetector CONSTRUCTOR"); // DEBUG
-    	
-    	isActivityRunning = true;
+	private float   mLastDirections[] = new float[3*2];
+	private float   mLastExtremes[][] = { new float[3*2], new float[3*2] };
+	private float   mLastDiff[] = new float[3*2];
+	private int     mLastMatch = -1;
 
-//    	TODO: API Level Detection (TYPE_STEP_COUNTER for API Level 19 and higher)
-//      Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+	private Vector observers;
 
-    	Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    	
-    	if (countSensor != null) {
-    		Log.i(TAG, "DEBUG - SUCCESS: Count sensoravailable"); // DEBUG
-    		sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-    	} else {
-    		Log.e(TAG, "DEBUG - ERROR: Count sensor not available"); // DEBUG
-//    		Toast.makeText(Context.SENSOR_SERVICE, "Count sensor not available!", Toast.LENGTH_LONG).show();
-    	}
-    }
-    
-    // ------------------------------------------------------------------------
-    
-    public void onSensorChanged(SensorEvent event) {
-    	//if(D) Log.d(TAG, "DEBUG - onSensorChanged"); // DEBUG
+	private int apiLevel;
 
-    	if(this.isActivityRunning){
-    		Sensor sensor = event.sensor; 
-    		synchronized (this) {
-    			if (sensor.getType() == Sensor.TYPE_ORIENTATION) {
-    			}
-    			else {
-    				int j = (sensor.getType() == Sensor.TYPE_ACCELEROMETER) ? 1 : 0;
-    				if (j == 1) {
-    					float vSum = 0;
-    					for (int i=0 ; i<3 ; i++) {
-    						final float v = mYOffset + event.values[i] * mScale[j];
-    						vSum += v;
-    					}
-    					int k = 0;
-    					float v = vSum / 3;
+	// ------------------------------------------------------------------------
 
-    					float direction = (v > mLastValues[k] ? 1 : (v < mLastValues[k] ? -1 : 0));
-    					if (direction == - mLastDirections[k]) {
-    						// Direction changed
-    						int extType = (direction > 0 ? 0 : 1); // minumum or maximum?
-    								mLastExtremes[extType][k] = mLastValues[k];
-    								float diff = Math.abs(mLastExtremes[extType][k] - mLastExtremes[1 - extType][k]);
+	public StepDetector(Object systemService) {
+		if(D)Log.i(TAG, "DEBUG - StepDetector CONSTRUCTOR"); // DEBUG
 
-    								if (diff > mLimit) {
+		this.sensorManager = (SensorManager) systemService;
 
-    									boolean isAlmostAsLargeAsPrevious = diff > (mLastDiff[k]*2/3);
-    									boolean isPreviousLargeEnough = mLastDiff[k] > (diff/3);
-    									boolean isNotContra = (mLastMatch != 1 - extType);
+		int h = 480; // TODO: remove this constant
+		mYOffset = h * 0.5f;
+		mScale[0] = - (h * 0.5f * (1.0f / (SensorManager.STANDARD_GRAVITY * 2)));
+		mScale[1] = - (h * 0.5f * (1.0f / (SensorManager.MAGNETIC_FIELD_EARTH_MAX)));
 
-    									if (isAlmostAsLargeAsPrevious && isPreviousLargeEnough && isNotContra) {
-    										if(D)Log.i(TAG, "step");
-    										
-    										//notify Observes
-    										this.notifyObserver();
-    										
-    										mLastMatch = extType;
-    									}
-    									else {
-    										mLastMatch = -1;
-    									}
-    								}
-    								mLastDiff[k] = diff;
-    					}
-    					mLastDirections[k] = direction;
-    					mLastValues[k] = v;
-    				}
-    			}
-    		}
-    	}
-    }
-    
-    // ------------------------------------------------------------------------
-    
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // TODO Auto-generated method stub
-    }
-    
-    // ------------------------- OBSERVER -------------------------------------
-    
+		// instantiate observer vector
+		observers = new Vector();
+	}
+
+	public void setSensitivity(float sensitivity) {
+		mLimit = sensitivity; // 1.97  2.96  4.44  6.66  10.00  15.00  22.50  33.75  50.62
+	}
+
+	// ------------------------------------------------------------------------
+
+	public void registerSensorManager(){
+		if(D)Log.i(TAG, "DEBUG - StepDetector CONSTRUCTOR"); // DEBUG
+
+		isActivityRunning = true;
+
+		//    	TODO: API Level Detection (TYPE_STEP_COUNTER for API Level 19 and higher)
+		this.apiLevel = Integer.valueOf(android.os.Build.VERSION.SDK);
+		if(D) Log.i(TAG, "Detected API version: " + apiLevel);
+
+		if(this.apiLevel < 19){
+			
+			Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+			if (countSensor != null) {
+				Log.i(TAG, "DEBUG - API Level < 19 - SUCCESS: Count sensoravailable"); // DEBUG
+				sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+			} else {
+				Log.e(TAG, "DEBUG - API Level < 19 - ERROR: Count sensor not available"); // DEBUG
+				// Toast.makeText(Context.SENSOR_SERVICE, "Count sensor not available!", Toast.LENGTH_LONG).show();
+			}
+		}
+		else{
+
+			Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+			if (countSensor != null) {
+				Log.i(TAG, "DEBUG - API Level >= 19 - SUCCESS: Count sensoravailable"); // DEBUG
+				sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_NORMAL);
+			} else {
+				Log.e(TAG, "DEBUG - API Level >= 19 - ERROR: Count sensor not available"); // DEBUG
+				// Toast.makeText(Context.SENSOR_SERVICE, "Count sensor not available!", Toast.LENGTH_LONG).show();
+			}
+		}
+
+
+
+	}
+
+	// ------------------------------------------------------------------------
+
+	public void onSensorChanged(SensorEvent event) {
+		//if(D) Log.d(TAG, "DEBUG - onSensorChanged"); // DEBUG
+
+		if(this.apiLevel < 19){
+
+			if(this.isActivityRunning){
+				Sensor sensor = event.sensor; 
+				synchronized (this) {
+					if (sensor.getType() == Sensor.TYPE_ORIENTATION) {
+					}
+					else {
+						int j = (sensor.getType() == Sensor.TYPE_ACCELEROMETER) ? 1 : 0;
+						if (j == 1) {
+							float vSum = 0;
+							for (int i=0 ; i<3 ; i++) {
+								final float v = mYOffset + event.values[i] * mScale[j];
+								vSum += v;
+							}
+							int k = 0;
+							float v = vSum / 3;
+
+							float direction = (v > mLastValues[k] ? 1 : (v < mLastValues[k] ? -1 : 0));
+							if (direction == - mLastDirections[k]) {
+								// Direction changed
+								int extType = (direction > 0 ? 0 : 1); // minumum or maximum?
+								mLastExtremes[extType][k] = mLastValues[k];
+								float diff = Math.abs(mLastExtremes[extType][k] - mLastExtremes[1 - extType][k]);
+
+								if (diff > mLimit) {
+
+									boolean isAlmostAsLargeAsPrevious = diff > (mLastDiff[k]*2/3);
+									boolean isPreviousLargeEnough = mLastDiff[k] > (diff/3);
+									boolean isNotContra = (mLastMatch != 1 - extType);
+
+									if (isAlmostAsLargeAsPrevious && isPreviousLargeEnough && isNotContra) {
+										if(D)Log.i(TAG, "step - apilevel < 19");
+
+										//notify Observes
+										this.notifyObserver();
+
+										mLastMatch = extType;
+									}
+									else {
+										mLastMatch = -1;
+									}
+								}
+								mLastDiff[k] = diff;
+							}
+							mLastDirections[k] = direction;
+							mLastValues[k] = v;
+						}
+					}
+				}
+			}
+		}
+		else{
+			if(D)Log.i(TAG, "step - apilevel >= 19");
+			this.notifyObserver();
+		}
+	}
+
+	// ------------------------------------------------------------------------
+
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+	}
+
+	// ------------------------- OBSERVER -------------------------------------
+
 	public void attachObserver(IStepDetectionObserver sdo){
-    	this.observers.addElement(sdo);
-    }
-    
-    public void detachObserver(IStepDetectionObserver sdo){
-    	observers.removeElement(sdo);
-    }
-    
-    public void notifyObserver(){
-    	for (int i=0; i< observers.size(); i++) {
+		this.observers.addElement(sdo);
+	}
+
+	public void detachObserver(IStepDetectionObserver sdo){
+		observers.removeElement(sdo);
+	}
+
+	public void notifyObserver(){
+		for (int i=0; i< observers.size(); i++) {
 			((IStepDetectionObserver)(observers.elementAt(i))).update();
 		}
-    }
-    
-    // -------------------------- SETTER / GETTER -----------------------------
-    
-    public void setActivityRunning(boolean b){
-    	this.isActivityRunning = b;
-    }
-    
-    public boolean getActivityRunning(){
-    	return this.isActivityRunning;
-    }
+	}
+
+	// -------------------------- SETTER / GETTER -----------------------------
+
+	public void setActivityRunning(boolean b){
+		this.isActivityRunning = b;
+	}
+
+	public boolean getActivityRunning(){
+		return this.isActivityRunning;
+	}
 
 }
